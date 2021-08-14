94684cbcee5389e2e4898028cdf6dd6737130aa7
Patricia Coelho <patriciacoelho.slv@gmail.com>
2021-08-14 14:49:27 -0300

    feat: Monta template da página principal da feature de configuração

--- client/src/features/history/pages/Main.vue
+++ client/src/features/history/pages/Main.vue
@@ -16,40 +16,6 @@
 					Passo 3
 				</template>
 			</custom-stepper> -->
-			<!-- <div>
-				<list-item
-					title="ÔNIBUS N7511-10"
-					expandable
-					@click="test"
-					@action-trigger="test"
-				>
-					<template v-slot:icon>
-						<v-img
-							max-height="50"
-							max-width="50"
-							:src="'/mark-bus.png'"
-						/>
-					</template>
-					<template v-slot:subtitle>
-						<p>Rota: Term. São Mateus</p>
-						<p>Motorista: Marcus Luiz</p>
-					</template>
-					<template v-slot:append-details>
-						<p>Detalhes da rota:  Via Av. Sapopemba (10 paradas)</p>
-						<p>Duração da rota:  45 minutos</p>
-						<p>UID do módulo embarcado:  0042</p>
-					</template>
-					<template v-slot:right-slot>
-						<p class="text-right">
-							ÚLTIMA TRANSMISSÃO <br>
-							<span>14:58:06</span>
-						</p>
-					</template>
-					<template v-slot:action>
-						EDITAR INFORMAÇÕES
-					</template>
-				</list-item>
-			</div> -->
 		</v-col>
 	</div>
 </template>
@@ -57,11 +23,9 @@
 <script>
 import PageHeader from '../../../core/components/PageHeader.vue';
 // import CustomStepper from '../../../core/components/CustomStepper.vue';
-// import ListItem from '../../../core/components/ListItem.vue';
 
 export default {
 	components: {
-		// ListItem,
 		// CustomStepper,
 		PageHeader,
 	},
@@ -87,9 +51,7 @@ export default {
 	},
 
 	methods: {
-		test() {
-			console.log('eia a som eu');
-		}
+		//
 	},
 }
 </script>
--- client/src/features/settings/pages/Main.vue
+++ client/src/features/settings/pages/Main.vue
@@ -1,3 +1,201 @@
 <template>
-	<h1>Settings Page</h1>
+	<v-col
+		class="pa-0"
+		cols="9"
+	>
+		<page-header
+			title="Configurações dos ônibus"
+			subtitle="Selecione um ônibus na lista de ônibus cadastrados para editar as informações"
+		>
+			<template v-slot:action>
+				<v-btn
+					class="px-8"
+					rounded
+					color="primary"
+					dark
+				>
+					<box-icon class="mr-2" name="plus" color="white" />
+					Adicionar ônibus
+				</v-btn>
+			</template>
+		</page-header>
+		<v-row class="mx-8 justify-space-between">
+			<v-col cols="6">
+				<v-text-field
+					v-model="searchQuery"
+					outlined
+					rounded
+					dense
+					placeholder="Pesquisar por código, rota ou motorista..."
+				>
+					<template v-slot:prepend-inner>
+						<box-icon name="search" color="#999999" />
+					</template>
+				</v-text-field>
+			</v-col>
+			<v-col
+				class="text-right"
+				cols="3"
+			>
+				<v-menu
+					offset-y
+					:close-on-content-click="false"
+				>
+					<template v-slot:activator="{ on, attrs }">
+						<v-btn
+							class="px-8"
+							rounded
+							outlined
+							height="40"
+							color="primary"
+							dark
+							v-bind="attrs"
+							v-on="on"
+						>
+							<box-icon class="mr-2" name="filter-alt" color="#2F2E3A" />
+							Filtrar
+						</v-btn>
+					</template>
+					<v-list>
+						<v-list-item>
+							<v-checkbox
+								v-model="showActiveModules"
+								class="mt-0 small"
+								label="Em circulação"
+								:hide-details="true"
+							/>
+						</v-list-item>
+						<v-list-item>
+							<v-checkbox
+								v-model="showInactiveModules"
+								class="mt-0"
+								label="Na garagem"
+								:hide-details="true"
+							/>
+						</v-list-item>
+						<v-list-item>
+							<v-checkbox
+								v-model="showNoSignalModules"
+								class="mt-0"
+								label="Sem sinal"
+								:hide-details="true"
+							/>
+						</v-list-item>
+					</v-list>
+				</v-menu>
+			</v-col>
+		</v-row>
+		<v-row class="mx-8">
+			<v-col cols="12">
+				<div
+					v-for="(item, i) in items"
+					:key="`onibus-${i}`"
+					class="mb-3"
+				>
+					<list-item
+						:title="item.title"
+						expandable
+						@click="test"
+						@action-trigger="test"
+					>
+						<template v-slot:icon>
+							<v-img
+								max-height="50"
+								max-width="50"
+								:src="`/${item.status}-marker.png`"
+							/>
+						</template>
+						<template v-slot:subtitle>
+							<p>Rota: {{ item.routeAlias }}</p>
+							<p>Motorista: {{ item.driverName }}</p>
+						</template>
+						<template v-slot:append-details>
+							<p>Detalhes da rota: {{ item.routeSpec }}</p>
+							<p>Duração da rota: {{ item.estimatedDuration }}</p>
+							<p>UID do módulo embarcado:  {{ item.uid }}</p>
+						</template>
+						<template v-slot:right-slot>
+							<div class="d-flex flex-column">
+								<div class="d-flex">
+									<p>
+										REDE #<span>{{ item.networkCode }}</span>
+									</p>
+								</div>
+								<div class="d-flex">
+									<p>
+										NET-ID: <span>{{ item.networkId }}</span>
+									</p>
+								</div>
+							</div>
+						</template>
+						<template v-slot:action>
+							EDITAR INFORMAÇÕES
+						</template>
+					</list-item>
+				</div>
+			</v-col>
+		</v-row>
+	</v-col>
 </template>
+
+<script>
+import PageHeader from '../../../core/components/PageHeader.vue';
+import ListItem from '../../../core/components/ListItem.vue';
+
+export default {
+	components: {
+		PageHeader,
+		ListItem,
+	},
+
+	data() {
+		return {
+			searchQuery: "",
+			showActiveModules: true,
+			showInactiveModules: true,
+			showNoSignalModules: true,
+			items: [
+				{
+					title: 'ÔNIBUS N7511-10',
+					status: 'watching',
+					routeAlias: 'Term. São Mateus',
+					routeSpec: 'Via Av. Sapopemba (10 paradas)',
+					driverName: 'Marcus Luiz',
+					estimatedDuration: '45 minutos',
+					uid: '0042',
+					networkCode: '0020',
+					networkId: '243',
+				},
+				{
+					title: 'ÔNIBUS NB519-12',
+					status: 'no-signal',
+					routeAlias: 'Consolação',
+					routeSpec: 'Via Av. Sapopemba (10 paradas)',
+					driverName: 'Marcus Luiz',
+					estimatedDuration: '45 minutos',
+					uid: '0042',
+					networkCode: '0020',
+					networkId: '127',
+				},
+				{
+					title: 'ÔNIBUS M7190-05',
+					status: 'in-yard',
+					routeAlias: 'Itaquera',
+					routeSpec: 'Via Av. Sapopemba (10 paradas)',
+					driverName: 'Marcus Luiz',
+					estimatedDuration: '45 minutos',
+					uid: '0042',
+					networkCode: '0020',
+					networkId: '042',
+				},
+			],
+		};
+	},
+
+	methods: {
+		test() {
+			console.log('eia a som eu');
+		},
+	},
+}
+</script>
