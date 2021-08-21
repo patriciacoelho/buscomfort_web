210f3fea7c030cfd482f8901a138bf12a234bf61
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2021-08-21 17:16:29 -0300

    refactor - separa componente do mapa da tela LiveMap

---
+++ client/src/features/live_map/components/LiveMap.vue
@@ -0,0 +1,219 @@
+<template>
+	<div>
+		<l-map
+			v-show="sideSheetCollapsed"
+			ref="overviewMap"
+			class="live-map__container live-map__container--expanded"
+			:center="center"
+			:zoom="zoomOverviewMap"
+			:options="{ zoomControl: false }"
+		>
+			<l-tile-layer
+				:url="url"
+				:attribution="attribution"
+			/>
+			<l-marker
+				v-for="marker in markers"
+				:key="`overviewMap-${marker.id}`"
+				:visible="marker.visible"
+				:lat-lng.sync="marker.position"
+				:icon="marker.icon"
+				@click="selectMarker(marker)"
+			/>
+			<l-control>
+				<v-card>
+					<v-card-text>
+						<span style="font-size: 1rem;">Status</span>
+						<v-checkbox
+							v-model="showActiveMarkers"
+							class="mt-0 small"
+							label="Em circulação"
+							:hide-details="true"
+						/>
+						<v-checkbox
+							v-model="showInactiveMarkers"
+							class="mt-0"
+							label="Na garagem"
+							:hide-details="true"
+						/>
+						<v-checkbox
+							v-model="showNoSignalMarkers"
+							class="mt-0"
+							label="Sem sinal"
+							:hide-details="true"
+						/>
+					</v-card-text>
+				</v-card>
+			</l-control>
+			<l-control-zoom position="bottomright" />
+		</l-map>
+		<l-map
+			v-show="!sideSheetCollapsed"
+			ref="detailMap"
+			class="live-map__container live-map__container--collapsed"
+			:center="centerDetailMap"
+			:zoom="zoomDetailMap"
+			:options="{ zoomControl: false }"
+		>
+			<l-tile-layer
+				:url="url"
+				:attribution="attribution"
+			/>
+			<div
+				v-for="marker in selectedMarker"
+				:key="`detailMap-${marker.id}`"
+			>
+				<l-marker
+					:visible="marker.visible"
+					:lat-lng.sync="marker.position"
+					:icon="marker.icon"
+				/>
+			</div>
+			<l-control>
+				<v-card>
+					<v-card-text>
+						<span style="font-size: 1rem;">Status</span>
+						<v-checkbox
+							v-model="showActiveMarkers"
+							class="mt-0 small"
+							label="Em circulação"
+							:hide-details="true"
+						/>
+						<v-checkbox
+							v-model="showInactiveMarkers"
+							class="mt-0"
+							label="Na garagem"
+							:hide-details="true"
+						/>
+						<v-checkbox
+							v-model="showNoSignalMarkers"
+							class="mt-0"
+							label="Sem sinal"
+							:hide-details="true"
+						/>
+					</v-card-text>
+				</v-card>
+			</l-control>
+			<l-control-zoom position="bottomright" />
+		</l-map>
+	</div>
+</template>
+
+<script>
+import {
+	LControlZoom,
+	LControl,
+	LMarker,
+} from 'vue2-leaflet';
+import { icon } from "leaflet";
+
+export default {
+	components: {
+		LControlZoom,
+		LControl,
+		LMarker,
+	},
+
+	props: {
+		collapsed: {
+			type: Boolean,
+			default: true,
+		},
+	},
+
+	data() {
+		return {
+			center: [-9.412170, -40.515131],
+			centerDetailMap: [-9.412170, -40.515131],
+			zoomOverviewMap: 13,
+			zoomDetailMap: 17,
+			attribution:
+				'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>',
+			url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+
+			showActiveMarkers: true,
+			showInactiveMarkers: true,
+			showNoSignalMarkers: true,
+			sideSheetCollapsed: true,
+
+			overviewMap: null,
+			detailMap: null,
+
+			markers: [
+				{
+					id: 'm1',
+					position: { lat: -9.413294, lng: -40.512148 },
+					visible: true,
+					icon: icon({
+						iconUrl: "watching-marker.png",
+						iconSize: [45, 45],
+						iconAnchor: [16, 37]
+					}),
+				},
+				{
+					id: 'm2',
+					position: { lat: -9.411403, lng: -40.512175 },
+					visible: true,
+					icon: icon({
+						iconUrl: "in-yard-marker.png",
+						iconSize: [45, 45],
+						iconAnchor: [20, 45]
+					}),
+				},
+			],
+			selectedMarker: [],
+		};
+	},
+
+	watch: {
+		collapsed: {
+			handler(newValue) {
+				this.sideSheetCollapsed = newValue;
+				this.selectedMarker = [];
+			},
+		},
+
+		sideSheetCollapsed: {
+			handler() {
+				setTimeout(() => {
+					this.detailMap.invalidateSize();
+					this.overviewMap.invalidateSize();
+				}, 100);
+			},
+			immediate: true,
+		}
+	},
+
+	mounted() {
+		this.$nextTick(() => {
+			this.overviewMap = this.$refs.overviewMap.mapObject;
+			this.detailMap = this.$refs.detailMap.mapObject;
+		});
+	},
+
+	methods: {
+		selectMarker(marker) {
+			this.centerDetailMap = marker.position;
+			this.selectedMarker.push(marker);
+			this.sideSheetCollapsed = !this.sideSheetCollapsed;
+			this.$emit('marker-selected', marker);
+		},
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+.live-map__container {
+	height: 100%;
+	width: 100%;
+	position: absolute;
+
+	&--collapsed {
+		width: calc(100% - 395px) !important;
+	}
+
+	&--expanded {
+		width: 100% !important;
+	}
+}
+</style>
--- client/src/features/live_map/pages/LiveMap.vue
+++ client/src/features/live_map/pages/LiveMap.vue
@@ -4,99 +4,10 @@
 			class="flex-grow-1"
 			style="height: 100%;"
 		>
-			<l-map
-				v-show="sideSheetCollapsed"
-				ref="overviewMap"
-				class="live-map__container live-map__container--expanded"
-				:center="center"
-				:zoom="zoom"
-				:options="{ zoomControl: false }"
-			>
-				<!-- @ready="doSomethingOnReady()" -->
-				<l-tile-layer
-					:url="url"
-					:attribution="attribution"
-				/>
-				<l-control>
-					<v-card>
-						<v-card-text>
-							<span style="font-size: 1rem;">Status</span>
-							<v-checkbox
-								v-model="showActiveMarkers"
-								class="mt-0 small"
-								label="Em circulação"
-								:hide-details="true"
-							/>
-							<v-checkbox
-								v-model="showInactiveMarkers"
-								class="mt-0"
-								label="Na garagem"
-								:hide-details="true"
-							/>
-							<v-checkbox
-								v-model="showNoSignalMarkers"
-								class="mt-0"
-								label="Sem sinal"
-								:hide-details="true"
-							/>
-						</v-card-text>
-					</v-card>
-				</l-control>
-				<l-control position="bottomleft"  class="example-custom-control">
-					<v-btn @click="sideSheetCollapsed = !sideSheetCollapsed">
-						Click me
-					</v-btn>
-					<!-- <p @click="showAlert">
-					Click me
-					</p> -->
-				</l-control>
-				<l-control-zoom position="bottomright" />
-			</l-map>
-			<l-map
-				v-show="!sideSheetCollapsed"
-				ref="detailMap"
-				class="live-map__container live-map__container--collapsed"
-				:center="center"
-				:zoom="zoom"
-				:options="{ zoomControl: false }"
-			>
-				<l-tile-layer
-					:url="url"
-					:attribution="attribution"
-				/>
-				<l-control>
-					<v-card>
-						<v-card-text>
-							<span style="font-size: 1rem;">Status</span>
-							<v-checkbox
-								v-model="showActiveMarkers"
-								class="mt-0 small"
-								label="Em circulação"
-								:hide-details="true"
-							/>
-							<v-checkbox
-								v-model="showInactiveMarkers"
-								class="mt-0"
-								label="Na garagem"
-								:hide-details="true"
-							/>
-							<v-checkbox
-								v-model="showNoSignalMarkers"
-								class="mt-0"
-								label="Sem sinal"
-								:hide-details="true"
-							/>
-						</v-card-text>
-					</v-card>
-				</l-control>
-				<!-- controller temporário -->
-				<l-control position="bottomleft"  class="example-custom-control">
-					<v-btn @click="sideSheetCollapsed = !sideSheetCollapsed">
-						Click me
-					</v-btn>
-				</l-control>
-				<l-control-zoom position="bottomright" />
-			</l-map>
+			<live-map
+				:collapsed="sideSheetCollapsed"
+				@marker-selected="handleMarkerSelection"
+			/>
 		</div>
 		<div
 			v-if="!sideSheetCollapsed"
@@ -108,57 +19,35 @@
 </template>
 
 <script>
-import { LControlZoom, LControl } from 'vue2-leaflet';
 import MoreInfo from '../components/MoreInfo.vue';
+import LiveMap from '../components/LiveMap.vue';
 
 export default {
 	components: {
-		LControlZoom,
-		LControl,
+		LiveMap,
 		MoreInfo,
 	},
 
 	data() {
 		return {
-			center: [-9.412170, -40.515131],
-			zoom: 13,
-			attribution:
-				'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>',
-			url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-
-			showActiveMarkers: true,
-			showInactiveMarkers: true,
-			showNoSignalMarkers: true,
 			sideSheetCollapsed: true,
-			overviewMap: null,
-			detailMap: null,
 		};
 	},
 
 	watch: {
-		sideSheetCollapsed: {
-			handler() {
-				setTimeout(() => {
-					this.detailMap.invalidateSize();
-					this.overviewMap.invalidateSize();
-				}, 100);
-			},
-			immediate: true,
-		}
-	},
-
-	mounted() {
-		this.$nextTick(() => {
-			this.overviewMap = this.$refs.overviewMap.mapObject;
-			this.detailMap = this.$refs.detailMap.mapObject;
-		});
+		//
 	},
 
 	methods: {
 		closeSideSheet(e) {
 			console.log(e);
 			this.sideSheetCollapsed = true;
-		}
+		},
+
+		handleMarkerSelection(e) {
+			console.log(e);
+			this.sideSheetCollapsed = false;
+		},
 	}
 };
 </script>
