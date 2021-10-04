0a07f50c9c14169528d31f989fa364e97b25c00b
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2021-10-04 08:52:21 -0300

    feat: add configuração do gráfico rangeBar

---
+++ client/src/features/history/constants/rangeBarChartConfig.js
@@ -0,0 +1,14 @@
+export default {
+  chart: {
+    type: 'rangeBar',
+    height: 350
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+};
